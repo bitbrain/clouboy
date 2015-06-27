d4dcbea2640b29f661f9c2427fd425149985bc19
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-06-27 13:06:04 +0200

    Initialized libgdx stuff

---
+++ desktop/src/de/bitbrain/clouboy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.bitbrain.clouboy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.bitbrain.clouboy.ClouBoy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClouBoy(), config);
+	}
+}
