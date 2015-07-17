3d477dcc1b61d68580b190f3fdc9dffdd54e6573
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-07-17 11:19:53 +0200

    Fixed achievement bug

--- core/src/de/bitbrain/clouboy/social/Achievement.java
+++ core/src/de/bitbrain/clouboy/social/Achievement.java
@@ -4,7 +4,7 @@ public final class Achievement {
 
     public static final String DATE_WITH_THE_DEVIL = "CgkItPm-vokbEAIQAw";
     public static final String CLOUD_PRESSURE = "CgkItPm-vokbEAIQBA";
-    public static final String CLOUD_INVADER = "CgkItPm-vokbEAIQBQ ";
+    public static final String CLOUD_INVADER = "CgkItPm-vokbEAIQBQ";
     public static final String CLOUD_MASTER = "CgkItPm-vokbEAIQBg";
     public static final String CLOU_BOY = "CgkItPm-vokbEAIQBw";
     public static final String MICHAEL_BAY = "CgkItPm-vokbEAIQCA";
